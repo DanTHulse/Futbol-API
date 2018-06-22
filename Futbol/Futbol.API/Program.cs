3e4ee9df93c3b813171f7ff8b555fcdfdb6906ad
Daniel Hulse <daniel.hulse@theleadagency.com>
2018-06-22 14:00:04 +0100

    Upgraded to NetCore2.1

--- Futbol/Futbol.API/Program.cs
+++ Futbol/Futbol.API/Program.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Futbol.API
 {
+    /// <summary>
+    /// The entry point of the program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The main entry point of the program
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        /// <summary>
+        /// Creates the web host builder.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
     }
 }
